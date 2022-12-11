cd1ce32caec703cb487430167aaa402f16c42292
Aashutosh Rathi <aashutoshrathi@gmail.com>
2022-12-11 22:23:13 +0530

    2022 - Day 10a

---
+++ 2022/Day10/day-10.js
@@ -0,0 +1,51 @@
+const fs = require("fs");
+
+const parseInputToObjects = (input) => {
+  const lines = input.split("\n");
+  return lines.map((line) => {
+    if (line === "noop") {
+      return [1, 0];
+    } else {
+      return [2, parseInt(line.split(" ")[1])];
+    }
+  });
+};
+
+const solveIt = (parsedInputs, isLarge) => {
+  const signalStrengths = [];
+  const measureCycles = [20, 60, 100, 140, 180, 220];
+
+  let measureCycleIndex = 0;
+  let currentCycle = 0;
+  let signalStrength = 1;
+
+  parsedInputs.forEach((input) => {
+    const [cyclesTaken, addition] = input;
+    if (currentCycle + cyclesTaken >= measureCycles[measureCycleIndex]) {
+      // console.log(measureCycles[measureCycleIndex], signalStrength);
+      signalStrengths.push(signalStrength * measureCycles[measureCycleIndex]);
+      measureCycleIndex++;
+    }
+    signalStrength += addition;
+    currentCycle += cyclesTaken;
+  });
+
+  return signalStrengths;
+};
+
+const main = (args) => {
+  const useSampleInput = args[0] === "y";
+  const useLargeInputs = args[1] === "b"; // can be or 'a' or 'b'
+
+  const input = fs
+    .readFileSync(`./${useSampleInput ? "sample-" : ""}input.txt`)
+    .toString();
+
+  const parsedInputs = parseInputToObjects(input);
+  const signalStrengths = solveIt(parsedInputs, useLargeInputs);
+  // console.log(signalStrengths);
+  const result = signalStrengths.reduce((a, b) => a + b, 0);
+  console.log(result);
+};
+
+main(process.argv.slice(2));
