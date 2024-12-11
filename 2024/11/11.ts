3ed999b6d543b177898c2203f23fb45c05df8b66
Aashutosh Rathi <aashutoshrathi@gmail.com>
2024-12-11 11:38:49 +0530

    2024: 11b - store count per stone, go deep once

--- 2024/11/11.ts
+++ 2024/11/11.ts
@@ -13,20 +13,29 @@ const getStoneNextState = (stone: string): string[] => {
 }
 
 const main = () => {
-  let ips = Deno.readTextFileSync(INPUT_FILE).split(" ");
+  const ips = Deno.readTextFileSync(INPUT_FILE).split(" ");
 
-  const NUMBER_OF_BLINKS = 25;
+  const NUMBER_OF_BLINKS = 75;
+  let uniqueStones: Map<string, number> = new Map();
+
+  // simply add duplicate stone counts here, iterate once for each
+  ips.forEach((ip) => {
+    uniqueStones.set(ip, (uniqueStones.get(ip) ?? 0) + 1);
+  });
 
   for (let i = 0; i < NUMBER_OF_BLINKS; i++) {
-    const newStones = [];
-    for (let j = 0; j < ips.length; j++) {
-      newStones.push(...getStoneNextState(ips[j]));
+    const nextUniqStones: Map<string, number> = new Map();
+    for (const [stone, count] of uniqueStones) {
+      const fStates = getStoneNextState(stone);
+      for (const nextState of fStates) {
+        nextUniqStones.set(nextState, (nextUniqStones.get(nextState) ?? 0) + count);
+      }
     }
-    ips = [...newStones];
+    uniqueStones = nextUniqStones;
   }
 
-  console.log(ips);
-  return ips.length;
+  const total = Array.from(uniqueStones.values()).reduce((acc, curr) => acc + curr, 0);
+  return total;
 }
 
 console.log(main())
