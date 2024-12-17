f54dce3b85a26c7ce9e72839d3fe03eb3c76c530
Aashutosh Rathi <aashutoshrathi@gmail.com>
2024-12-17 19:10:13 +0530

    add printMaze

--- 2024/16/16a.ts
+++ 2024/16/16a.ts
@@ -1,6 +1,7 @@
 const INPUT_FILE = Deno.args[0] === "prod" ? "./main_input.txt" : "./input.txt";
 
 const INT_MAX = Infinity;
+
 const main = () => {
   const inputs = Deno.readTextFileSync(INPUT_FILE).split("\n");
   const maze = inputs.map((row) => row.split(""));
@@ -14,12 +15,36 @@ const main = () => {
     }
   }
 
-  const DIR_TO_MOVEMENT: Record<number, number[]> = {
-    0: [-1, 0],
-    1: [0, 1],
-    2: [1, 0],
-    3: [0, -1]
+  function printMaze(pt: number[]) {
+    console.log("\x1Bc")
+    const temp = Array.from({ length: maze.length }, () => Array.from({ length: maze[0].length }, () => ""));
+
+    for (let i = 0; i < maze.length; i++) {
+      for (let j = 0; j < maze[i].length; j++) {
+        if (pt[0] === i && pt[1] === j) {
+          temp[i][j] = "@"
+        } else {
+          temp[i][j] = maze[i][j]
+        }
+      }
+    }
+
+    console.log(temp.map(row => row.join("")).join("\n"));
+    let wait = 100;
+    if (maze[pt[0]][pt[1]] === "E") {
+      wait = 1000;
+    }
+    const waitTill = new Date(new Date().getTime() + wait);
+    while (waitTill > new Date()) { }
+    console.log("\n")
   }
+
+  const DIR_TO_MOVEMENT: Record<number, number[]> = {
+    0: [-1, 0],  // UP
+    1: [0, 1],   // RIGHT
+    2: [1, 0],   // DOWN
+    3: [0, -1]   // LEFT
+  };
 
   const cache: Record<string, number> = {}
 
@@ -37,33 +62,30 @@ const main = () => {
       return INT_MAX;
     }
 
-    if (cache[cacheKey]) {
-      return cache[cacheKey];
-    }
+    // printMaze([x, y])
 
     // End condition
     if (maze[x][y] === "E") {
       return 0;
     }
 
+    if (cache[cacheKey]) {
+      return cache[cacheKey];
+    }
     // Mark position as visited
     visited.add(posKey);
-
-    // Try moves: straight, right turn, left turn
     const availableMoves = [currentFace, (currentFace + 1) % 4, (currentFace + 3) % 4];
 
     let min = INT_MAX;
     for (const move of availableMoves) {
       const [dx, dy] = DIR_TO_MOVEMENT[move];
       const nextPos = [x + dx, y + dy];
-      // Create new visited set for each path
+
       const nextVisited = new Set(visited);
       const res = walkTheMaze(nextPos, move, 0, nextVisited);
-      if (res !== INT_MAX) {
-        const nextScore = res + score + (move !== currentFace ? 1001 : 1);
-        // console.log(`At ${x},${y} with face ${currentFace} trying move ${move} to ${nextPos} with score ${nextScore} got ${res}`)
-        min = Math.min(min, nextScore);
-      }
+      // printMaze([x, y])
+      const nextScore = res + score + (move !== currentFace ? 1001 : 1)
+      min = Math.min(min, nextScore);
     }
 
     visited.delete(posKey);
@@ -72,7 +94,6 @@ const main = () => {
   }
 
   const res = walkTheMaze(start, 1, 0)
-  // console.log(visited.map((row) => row.map((cell) => cell ? "X" : ".").join("")).join("\n"))
   return res;
 }
 console.log(main());
